faf1136314c634c8080689347117940cdbddf228
Shahi Raj <sean66raj@gmail.com>
2021-06-26 15:38:25 -0400

    First commit

---
+++ Linux/forloop.sh
@@ -0,0 +1,29 @@
+﻿#!/bin/bash
+
+
+provinces =('Ontario' 'Quebec' 'Alberta' 'BC' 'Qu')
+
+
+for province in $(provinces)[@]}
+do
+if [ $provinces == 'Alberta' ];
+then
+        echo "Alberta is the best"
+else
+        echo "I am not a fan of Alberta"
+fi
+
+
+
+
+
+
+done
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+"~/for_loop.sh" 16L, 224C                                     1,1           All
---
+++ Linux/script1.sh
@@ -0,0 +1,10 @@
+﻿#!/bin/bash
+#checks for the existence of the ~/research directory
+
+
+if [ -d ~/research ] 
+then 
+     echo "Directory exists"
+else
+     mkdir ~/research 
+fi
